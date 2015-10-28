ca7e2f39760f3f1922a513d0d8bf0fa04d103bf0
Donnie Tognazzini <donnie.tognazzini@appfolio.com>
2015-10-28 12:03:25 -0700

    persist token for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,20 @@ class User < ActiveRecord::Base
   devise :omniauthable, :omniauth_providers => [:github]
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    user = where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.email = auth.info.email
+      user.token = auth.credentials.token
       user.password = Devise.friendly_token[0,20]
     end
+
+    token = auth.credentials.token
+
+    if token != user.token
+      user.update_attributes!(token: token)
+    end
+
+    user
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "from_omniauth updates token" do
+    user = User.create!(email: 'bleh@bleh.com', password: 'blehbleh', provider: "bleh", uid: "123", token: 'a')
+    auth = mock(provider: user.provider, uid: user.uid, credentials: mock(token: 'B'))
+
+    user_from_omniauth = User.from_omniauth(auth)
+
+    assert_equal user, user_from_omniauth
+    assert_equal 'B', user_from_omniauth.token
+  end
 end
