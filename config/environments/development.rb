65b1b7137091b50498e1be90029f8f8b4b298ce1
Donnie Tognazzini <donnie.tognazzini@appfolio.com>
2015-11-02 13:12:24 -0800

    Use SendGrid in production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,15 +36,5 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               ENV['GMAIL_DOMAIN'],
-    user_name:            ENV['GMAIL_USERNAME'],
-    password:             ENV['GMAIL_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
