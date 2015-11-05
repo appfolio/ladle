e95486682d6ddbd6fcfa50534232b035d0a05657
Donnie Tognazzini <donnie.tognazzini@appfolio.com>
2015-11-04 17:36:26 -0800

    Fixing UserMailer mishap

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   default from: 'Appfolio Ladle <no-reply@appfolio.com>'
 
   def notify(user:, repository:, pull_request:, stewards_files:)
-    pull_request.assert_valid_keys(:url, :description, :title)
+    pull_request.assert_valid_keys(:number, :url, :description, :title)
     raise ArgumentError, "pull_request[:url] required" if pull_request[:url].blank?
 
     pull_request = pull_request.dup
