3a10a97e7168e7bd5154913d97fb0dda03026514
Donnie Tognazzini <donnie.tognazzini@appfolio.com>
2015-11-04 17:42:40 -0800

    Fixing mailer host configuration

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module Ladle
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
     def github_config
       @github_config ||= GithubConfig.from_values(config_for(:github))
     end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: ENV['MAILER_HOST'] }
+
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
 
