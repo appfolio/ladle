4181206bb985ed426303ce67fde93d22c77ed339
Donnie Tognazzini <donnie.tognazzini@appfolio.com>
2015-11-05 14:44:37 -0800

    Replacing simplecov with coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
-require 'simplecov'
-SimpleCov.start 'rails'
-SimpleCov.minimum_coverage 100
+
+require 'coveralls'
+Coveralls.wear!('rails')
 
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
