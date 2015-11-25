948946a1314b4fa9f1dfe543ce7ab2257f9328f9
Donnie Tognazzini <donnie.tognazzini@appfolio.com>
2015-11-25 14:25:53 -0800

    Enhance StewardsFile.parse with support for includes/excludes glob patterns

--- lib/ladle/stewards_file.rb
+++ lib/ladle/stewards_file.rb
@@ -3,11 +3,25 @@ module Ladle
     attr_reader :stewards
 
     class Steward
-      attr_reader :github_username
+      attr_reader :github_username, :include_patterns, :exclude_patterns
 
-      def initialize(github_username)
+      def initialize(github_username:, include_patterns: nil, exclude_patterns: nil)
         @github_username = github_username
+        @include_patterns = include_patterns || []
+        @exclude_patterns = exclude_patterns || []
       end
+
+      def ==(other)
+        @github_username == other.github_username &&
+          @include_patterns == other.include_patterns &&
+          @exclude_patterns == other.exclude_patterns
+      end
+
+      def hash
+        [@github_username, @include_patterns, @exclude_patterns].hash
+      end
+
+      alias eql? ==
     end
 
     def initialize(stewards = [])
@@ -15,8 +29,20 @@ module Ladle
     end
 
     def self.parse(contents)
-      stewards = YAML.load(contents)["stewards"].map do |steward_config|
-        Steward.new(steward_config)
+      raise "Cannot parse empty file" if contents.blank?
+
+      yaml_contents = YAML.load(contents)
+      stewards = (yaml_contents["stewards"] || []).map do |config|
+        github_username = config
+        include_patterns = []
+        exclude_patterns = []
+
+        if config.is_a?(Hash)
+          github_username = config["github_username"]
+          include_patterns = config["include"]
+          exclude_patterns = config["exclude"]
+        end
+        Steward.new(github_username: github_username, include_patterns: include_patterns, exclude_patterns: exclude_patterns)
       end
 
       new(stewards)
